f5bb7300b8a10d37e4f69269d4b22d647fc5a402
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-12 10:31:17 +0200

    add initial comments and structure to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// creo un array di obj con le seguenti proprietà:
+// - img, titolo, descrizione
+
+// seleziono gli elementi del DOM che mi servono
+// - prev, next, image, thumbnail, 
+
+// renderizzo l'img principale nel dom
+
+// genero il markup delle thumbnail
+
